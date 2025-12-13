b30f9c6d5e7711d156f23244f29fcbdb02d7fd30
Adjifatou30 <adjifatoudiop@esp.sn>
2025-12-14 00:11:44 +0100

    Initial commit: create ImplementInterfaces project with Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P4ImplementInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
